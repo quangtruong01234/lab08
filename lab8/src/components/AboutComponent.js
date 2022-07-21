d6fea01d424a85fc5afa9ffb381beabd0b4b684d
Truong <quangtruong01234@gmail.com>
2022-07-21 11:45:00 +0700

    lab8

--- lab8/src/components/AboutComponent.js
+++ lab8/src/components/AboutComponent.js
@@ -25,7 +25,7 @@ function RenderLeader ({ leader, isLoading, errMess }) {
         <div key={ leader.id } className="col-12 mt-2">
             <Media tag="li">
                 <Media left middle>
-                    <span class="d-flex mr-5 img-thumbnail 1324567891234">
+                    <span class="d-flex mr-5 img-thumbnail">
                         <Media object src={ baseUrl + leader.image } alt={ leader.name } />
                     </span>
                 </Media>
